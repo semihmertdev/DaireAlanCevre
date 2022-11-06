dd634c79db1120aca2b93e21040256e1764a68b0
semihmertdev <semihmert.dev@gmail.com>
2022-11-06 17:23:58 +0300

    .

--- src/DaireAlanCevre.java
+++ src/DaireAlanCevre.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+
 public class DaireAlanCevre {
     public static void main(String[]args) {
-        int r;
+        
+    	int r;
         double pi=3.14,alan,alfa;
+        
         Scanner inp = new Scanner(System.in);
         System.out.print("Dairenin yaricapini giriniz:");
         r=inp.nextInt();
